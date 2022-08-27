b51459cf862c177dda77b6851364a2b8c6ca849c
baonguyen96 <baonguyendrump@gmail.com>
2022-08-27 10:52:08 -0500

    Start code coverage

--- CreationalPatterns/AbstractFactory/src/test/java/MainTest.java
+++ CreationalPatterns/AbstractFactory/src/test/java/MainTest.java
@@ -1,16 +1,34 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
     @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
     }
 
     @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+    public void tearDown() {
+        System.setOut(System.out);
+        System.setErr(System.err);
     }
 
     @org.junit.jupiter.api.Test
-    void main() {
+    public void main() {
+        Main.main(new String[]{""});
+        String expected = "class factory.MacOsFactory\n" +
+                "MacKeyboard has Command-Option\n" +
+                "MacTerminal for life\n" +
+                "\n" +
+                "class factory.WindowsOsFactory\n" +
+                "Windows keyboard has CTL-FN-Win\n" +
+                "Powershell is king";
+        assertEquals(expected, outContent.toString().trim().replace("\r", ""));
     }
 }
--- README.md
+++ README.md
@@ -1,20 +1,26 @@
 # GoF Design Patterns
 
+
+## Introduction
+This repository is collection of small demos for each GoF design patterns.
+
+
 ## Setup
 1. Java11 or above
+2. Tests with JUnit5
+
 
 ## Patterns
-### [Creational Patterns](./CreationalPatterns)
 
+### [Creational Patterns](./CreationalPatterns)
 - [Abstract Factory](./CreationalPatterns/AbstractFactory/src/main/java/Main.java): interface for creating families of related/dependent objects without specifying their concrete implementations
 - [Builder](./CreationalPatterns/Builder/src/main/java/Main.java): how a composite object gets created 
 - [Factory Method](./CreationalPatterns/FactoryMethod/src/main/java/Main.java): subclass of object that is instantiated
 - [Prototype](./CreationalPatterns/Prototype/src/main/java/Main.java): class of object that is instantiated 
 - [Singleton](./CreationalPatterns/Singleton/src/main/java/Main.java): the sole instance of a class 
 
-
 ### [Behavioral Patterns](./BehavioralPatterns)
-
+- TBD
 
 ### [Structural Patterns](./StructuralPatterns)
-
+- TBD
